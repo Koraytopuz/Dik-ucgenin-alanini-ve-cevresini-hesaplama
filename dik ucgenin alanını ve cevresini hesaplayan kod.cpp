db6e74676469ae1c61f74ec449418369ea966891
Koraytopuz <91742967+Koraytopuz@users.noreply.github.com>
2023-03-10 21:13:21 +0300

    Add files via upload

---
+++ dik ucgenin alanını ve cevresini hesaplayan kod.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{	
+	int a,b,c;
+	cout<<"a kenarini giriniz"<< endl;
+	cin>>a ;
+	cout<<"b kenarini giriniz"<< endl;
+	cin>>b;
+	c=sqrt(a*a+b*b);
+	cout<<"h="<<c<< endl;
+	cout<<"cevresi="<<a+b+c<< endl;
+	cout<<"alani="<<float(a*b/2)<< endl;
+}
